61c6a8b9672153d2c0627627f159d1a6846826cf
noemileon396-dot <noemileon396@gmail.com>
2026-02-13 18:22:24 -0600

    Update photo filename in script.js

--- script.js
+++ script.js
@@ -130,7 +130,7 @@ const recuerdos = [
         info: "Novia feliz, vida feliz."
     },
     {
-        foto: "besitos.jpg",
+        foto: "foto%20(158).JPG",
         info: "Besitos."
     },
     {
